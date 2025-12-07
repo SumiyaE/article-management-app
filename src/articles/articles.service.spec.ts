46ad6dca8f4c5e39cf9d3307ff5011a6c91e8997
SumiyaE <s_eguchi@codmon.co.jp>
2025-12-07 15:38:59 +0900

    fix: #articlesのAPIを作成

--- src/articles/articles.service.spec.ts
+++ src/articles/articles.service.spec.ts
@@ -3,6 +3,7 @@ import { ArticlesService } from './articles.service';
 
 describe('ArticlesService', () => {
   let service: ArticlesService;
+  let expectedResponce: { id: number; title: string; content: string }[];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -10,9 +11,31 @@ describe('ArticlesService', () => {
     }).compile();
 
     service = module.get<ArticlesService>(ArticlesService);
+
+    // 記事一覧のインメモリ初期データ
+    expectedResponce = [{
+      id: 1,
+      title: '最初の投稿',
+      content: 'これは初めての投稿です。'
+    },
+    {
+      id: 2,
+      title: '二つ目の投稿',
+      content: 'これは二つ目の投稿です'
+    }
+    ]
   });
 
   it('Serviceが定義されていること', () => {
     expect(service).toBeDefined();
+  });
+
+  it('findAll()が記事の一覧を返すこと', () => {
+    expect(service.findAll()).toEqual(expectedResponce);
+  });
+
+  it('findOne()が指定したIDの記事を返すこと', () => {
+    expect(service.findOne(1)).toEqual(expectedResponce[0]);
+    expect(service.findOne(2)).toEqual(expectedResponce[1]);
   });
 });
--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -4,16 +4,28 @@ import { UpdateArticleDto } from './dto/update-article.dto';
 
 @Injectable()
 export class ArticlesService {
+  readonly articles: { id: number; title: string; content: string }[] = [
+    {
+      id: 1,
+      title: '最初の投稿',
+      content: 'これは初めての投稿です。',
+    },
+    {
+      id: 2,
+      title: '二つ目の投稿',
+      content: 'これは二つ目の投稿です',
+    },
+  ];
   // create(createArticleDto: CreateArticleDto) {
   //   return 'This action adds a new article';
   // }
 
   findAll() {
-    return `This action returns all articles`;
+    return this.articles;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} article`;
+    return this.articles.find((article) => article.id === id);
   }
 
   // update(id: number, updateArticleDto: UpdateArticleDto) {
